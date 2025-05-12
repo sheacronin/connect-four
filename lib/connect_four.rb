7a6873d2ceadd7ba867769757e2290933212faa9
Shea Cronin <shealcronin@gmail.com>
2025-05-11 20:02:27 -0400

    add false test and horizontal row test for #game_over?

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -18,4 +18,25 @@ class ConnectFour
   def place_token(row, col)
     @grid[row][col] = '◍'
   end
+
+  def game_over?
+    return true if grid.any? do |row|
+      tokens_with_index = row.each_with_index.reject do |space, i|
+        space.nil?
+      end
+
+      current_index = nil
+      tokens_in_a_row = 0
+      tokens_with_index.each do |_token, i|
+        tokens_in_a_row += 1 if current_index == i - 1
+        return true if tokens_in_a_row == 3
+
+        current_index = i
+      end
+
+      false
+    end
+
+    false
+  end
 end
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -27,4 +27,19 @@ describe ConnectFour do
                               [nil, nil, nil, nil, nil, nil, nil]
                             ])
   end
+
+  describe '#game_over?' do
+    it 'returns false when there are not 4 tokens in a row' do
+      game.place_token(0, 0)
+      expect(game.game_over?).to be(false)
+    end
+
+    it 'returns true when 4 tokens are in a horizontal row' do
+      game.place_token(0, 0)
+      game.place_token(0, 1)
+      game.place_token(0, 2)
+      game.place_token(0, 3)
+      expect(game.game_over?).to be(true)
+    end
+  end
 end
