cd557c0ab89632c889590804d8dd4cf9cbf8b2de
Shea Cronin <shealcronin@gmail.com>
2025-06-02 10:59:43 -0400

    Add Players to ConnectFour and start writing #play method

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -4,8 +4,24 @@ require_relative './board'
 
 # Game where players try to place four tokens in a row
 class ConnectFour
-  def initialize(board = Board.new)
+  attr_reader :players
+
+  def initialize(players:, board: Board.new)
+    @players = players
     @board = board
+  end
+
+  # Play game
+  # who's turn is it?
+  # they select a space to place a token (change this later to drop)
+  # check for game over?
+  # if not, change whose turn and continue play
+  # if yes, handle game over
+  def play
+    column = $stdin.gets.chomp.to_i
+    @board.place_token(0, column)
+    @board.show_grid
+    game_over?
   end
 
   def game_over?
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -2,10 +2,39 @@
 
 require_relative '../lib/connect_four'
 require_relative '../lib/board'
+require_relative '../lib/player'
 
 describe ConnectFour do
   let(:board) { instance_double(Board) }
-  subject(:game) { described_class.new(board) }
+  let(:players) { Array.new(2, instance_double(Player)) }
+  subject(:game) { described_class.new(players:, board:) }
+
+  it 'has 2 players' do
+    expect(game.players).to be(players)
+  end
+
+  it 'has a board' do
+    expect(game.instance_variable_get(:@board)).to be(board)
+  end
+
+  describe '#play' do
+    before do
+      allow($stdin).to receive(:gets).and_return('1')
+      allow(board).to receive(:place_token)
+      allow(board).to receive(:show_grid)
+      allow(board).to receive(:any_winning_sequence?).and_return(false)
+    end
+
+    it 'places a token on the board' do
+      expect(board).to receive(:place_token).with(0, 1)
+      game.play
+    end
+
+    it 'checks if the game is over' do
+      expect(game).to receive(:game_over?)
+      game.play
+    end
+  end
 
   describe '#game_over?' do
     it 'returns false when there is not a winning sequence' do
